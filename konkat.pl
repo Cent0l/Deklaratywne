5c3c8988ed14a0cacd187b10324c92d0b3ec1c90
Mateusz <115219630+Cent0l@users.noreply.github.com>
2024-04-03 14:32:53 +0200

    Add files via upload

---
+++ konkat.pl
@@ -0,0 +1,25 @@
+konkat([],L,L).
+
+konkat([H1|T1],L2,[H1|T3]):-konkat(T1,L2,T3).
+
+odwrotna_lista([],[]).
+
+odwrotna_lista([H|T1],L):-odwrotna_lista(T1,T2),konkat(T2,[H],L).
+
+dodatni(X):-X>0.
+
+dodatnia_lista(L)
+				:-maplist(dodatni,L).
+
+wiekszy_od(X,Y):-Y>X.
+
+wiekszy_od_lista(X,L):-
+				maplist(wiekszy_od(X),L).
+		
+		
+		
+kwadrat(X,Y):-
+				Y is X*X.
+
+kwadrat_lista(L1,L2):-
+				maplist(kwadrat,L1,L2).
--- wielomiany.pl
+++ wielomiany.pl
@@ -1,14 +1,4 @@
-% rozpoznawanie wielomianow o wspolczynnikach liczbowych
-/*
-Definicja: rekurencja strukturalna
-1) 
-x jest wielomianem dowolnej zmiennej x
-c- liczba jest wielomianem dowolnej zmiennej
-2)
-Jezeli w, W1, W2 sa wielomiannamie zmiennej x, to
--W, W1+W1,W1-W2, W1*W2, W^N (Naturalna)
-sa wielomianami zmiennej x
-*/%lab 05
+%lab 05
 %maximum X1 i X2
 /*
 działania
@@ -89,27 +79,3 @@ st(W1*W2)=st(W1)+st(W2),st(W^N)=st(W)*N (N-liczba naturalna, N>1)
 %rekurencja
 	st_wielomian(-W,X,X):-st_wielomian(W,X,X).
 		st_wielomian(W1+W2,X,X):-st_wielomaian()
-
-% wielomian(W,X) 
-% jest spełnony gdy W jest wielomianem zmiennej x
-% o współczynnikach liczbowych
-
-% def rekurencyjna
-
-% warunki zakonczenia rekurencji
-	wielomian(X,X).
-	wielomian(C,_):-numer(C).
-	
-	% rekurencja
-		wielomian(-W,X):-wielomian(W,X).
-		wielomian(W1+W2,X):-wielomian(W1,X), wielomian(W2,X).
-		wielomian(W1-W2,X):-wielomian(W1,X), wielomian(W2,X).
-		wielomian(W1*W2,X):-wielomian(W1,X), wielomian(W2,X).
-		wielomian(W^N,X):-integer(N), N>1, wielomian(W,X).
-		
-		
-		
-/*
-1. wielomian(y,y).
-1.wielomian(5,Y).
-*/
