90ba5395686297fafeffdb73579c40eacaf0ea0b
Mateusz <115219630+Cent0l@users.noreply.github.com>
2024-04-03 14:36:09 +0200

    Update konkat.pl

--- konkat.pl
+++ konkat.pl
@@ -8,18 +8,18 @@ odwrotna_lista([H|T1],L):-odwrotna_lista(T1,T2),konkat(T2,[H],L).
 
 dodatni(X):-X>0.
 
-dodatnia_lista(L)
-				:-maplist(dodatni,L).
+dodatnia_lista(L):-maplist(dodatni,L).
 
 wiekszy_od(X,Y):-Y>X.
 
-wiekszy_od_lista(X,L):-
-				maplist(wiekszy_od(X),L).
+wiekszy_od_lista(X,L):-maplist(wiekszy_od(X),L).
 		
 		
 		
-kwadrat(X,Y):-
-				Y is X*X.
+kwadrat(X,Y):-Y is X*X.
 
-kwadrat_lista(L1,L2):-
-				maplist(kwadrat,L1,L2).
+kwadrat_lista(L1,L2):-maplist(kwadrat,L1,L2).
+
+razy2(X,Y):-Y is X*2.
+			
+razy2_lista(L1,L2):-maplist(razy2,L1,L2).
